ec59ac35af3b53a21f459b943828559b9cea3fdd
abinav04 <abhinavsosa23@gmail.com>
2025-09-17 11:00:21 +0530

    Updated env file

--- backend/controllers/recordController.js
+++ backend/controllers/recordController.js
@@ -24,6 +24,7 @@ export const getRecords = async (req,res)=>{
       const records = await Record.find(query);
       res.json(records);
     }catch(e){
+          console.error("Error in getRecords:", e.message);
         res.status(500).json({message:e.message});
     }
 }
